c5b616fae9eb6e317fd5ef5e4159e9230c6837e8
Paarth Karingula <paarthkaringula@gmail.com>
2025-09-07 11:05:45 +0530

    Removed old unused folder

--- IIDPS/Django/IIDPS/webapp/DBConnection.py
+++
@@ -1,8 +0,0 @@
-import mysql.connector
-class DBConnection:
-    @staticmethod
-    def getConnection():
-        database = mysql.connector.connect(host="localhost", user="root", passwd="root", db='IIDPS')
-        return database
-if __name__=="__main__":
-    print(DBConnection.getConnection())
